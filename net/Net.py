4e84dbbb3769ca74ee04d0db3552608f783095e8
Bas <basvolkers99@gmail.com>
2022-06-01 14:32:59 +0200

    Two output values

--- net/Net.py
+++ net/Net.py
@@ -67,7 +67,7 @@ class Net(nn.Module):
             'lstm': nn.LSTM(input_size=self.config["lstm"]["in"], hidden_size=self.config["lstm"]["hidden"]),
             # 'lstm': LSTM(input_size=self.config["lstm"]["in"], hidden_size=self.config["lstm"]["hidden"]),
             'out': nn.Sequential(
-                nn.Linear(self.config["lstm"]["hidden"], 1)#self.config["output"]["size"]),
+                nn.Linear(self.config["lstm"]["hidden"], self.config["output"]["size"]),
                 # nn.Softmax(dim=1)
             )
         })
