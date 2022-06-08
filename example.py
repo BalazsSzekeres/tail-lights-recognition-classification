9011e453356c172fc9604d746c158fa6dd8d7258
bramvanriessen <bramvriessen@gmail.com>
2022-06-08 17:14:28 +0200

    Updated example.py, now loads weights

--- example.py
+++ example.py
@@ -9,28 +9,28 @@ from torchvision import transforms
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-path = "/home/bas/Documents/Master/Y1/Q4/Seminar Computer Vision by Deep " \
-       "Learning/project/rear_signal_dataset_copy/20160805_g1k17-08-05-2016_15-57-59_idx99/20160805_g1k17-08-05" \
-       "-2016_15-57-59_idx99_BOO/20160805_g1k17-08-05-2016_15-57-59_idx99_BOO_00002671/light_mask"
+path = "/home/bramvr/datasets/rear_signal_dataset_sampleset/lighting"
+
 transform = transforms.Compose([transforms.Resize(size=(227, 227)),
                                 transforms.ToTensor()])
 images = []
 for i, f in enumerate(glob.iglob(f"{path}/*.png")):
-    if i == 10:
+    if i == 1:
         break
     images.append(transform(Image.open(f)))
 input = torch.stack(images).to(device)
 
-config_file = "config/net.yaml"
+config_file = "config/net/net.yaml"
 config = yaml.load(open(config_file), Loader=yaml.FullLoader)
 
 net = Net(config).to(device)
 summary(net.model['cnn'], (config["input"]["dim"], config["input"]["size"], config["input"]["size"]))
 
+checkpoint = torch.load('weights/weights_29.pt', map_location=device)
+net.load_state_dict(checkpoint['model'])
+
 # Example usage
 y_list = net.forward_sequence_list([input, input])
 y1 = net(input)
-y2 = net(input)
 print(y1)
-print(y2)
 print(y_list)
